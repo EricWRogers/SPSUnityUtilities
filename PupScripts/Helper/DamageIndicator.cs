7aea8b0000c67658a9d0a86f59f2ef0f2a6cfd3b
EricWRogers <ericwilliamrogers@gmail.com>
2024-09-09 15:26:50 -0500

    Simple Damage Indicator

---
+++ PupScripts/Helper/DamageIndicator.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SuperPupSystems.Helper
+{
+    public class DamageIndicator : MonoBehaviour
+    {
+        public Material defaultMaterial;
+        public Material hurtMaterial;
+        public Timer timer;
+        
+        public void ResetDefaultMaterial()
+        {
+            gameObject.GetComponent<Renderer>().material = defaultMaterial;
+        }
+
+        public void Hurt()
+        {
+            timer.Start();
+            gameObject.GetComponent<Renderer>().material = hurtMaterial;
+        }
+    }
+}
